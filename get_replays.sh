11c56ac89ab76d6f940b230f1ec4a6474e3225d8
s174437 <s174437@student.dtu.com>
2022-05-18 10:57:05 +0200

    Visualization improved

--- get_replays.sh
+++ get_replays.sh
@@ -5,7 +5,7 @@ MAP="lbforaging:Foraging-12x12-2p-3f-v2"
 for i in {0..10}
 do
 	#python3 src/main.py --config=iql --env-config=gymma with env_args.time_limit=50 env_args.key="lbforaging:Foraging-8x8-2p-3f-v2"
-	python3 src/main.py --config=iql --env-config=gymma with env_args.time_limit=25 env_args.key=$MAP
+	python3 src/main.py --config=iql --env-config=gymma with env_args.time_limit=50 env_args.key=$MAP
 
 done
 
